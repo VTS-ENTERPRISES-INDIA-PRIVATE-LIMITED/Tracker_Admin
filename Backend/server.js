805cb60c36952660b087ca0f5fe1de0ff17edbbd
HemaAmruthaDasari <dasariamrutha3@gmail.com>
2024-09-30 12:46:28 +0530

    route

--- Backend/server.js
+++ Backend/server.js
@@ -3,11 +3,11 @@ const app=express();
 const env = require('dotenv').config();
 const connection=require('./db');
 const cors = require('cors');
-
+const attendance = require('./Routes/attendance')
 app.use(cors({ origin: '*' }));
-
-
 app.use(express.json());
+app.use('/attendance',attendance);
+
 
 app.listen(5000,()=>{
     console.log("running on port:",5000)
