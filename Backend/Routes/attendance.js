0f1ee95f9d9a7f7ba3911ae57733be88d0e6a386
HemaAmruthaDasari <dasariamrutha3@gmail.com>
2024-09-30 12:41:23 +0530

    attendance

--- Backend/Routes/attendance.js
+++ Backend/Routes/attendance.js
@@ -15,7 +15,7 @@ router.post('/postAttendance',async(req,res)=>{
             const month = String(date.getMonth() + 1).padStart(2, '0');
             const day = String(date.getDate()).padStart(2, '0');
             return `${day}/${month}/${year}`;
-        }
+}
         await connection.query(createQueries.PostAttendance,[Name,EmpId,Shift,Breakfast,Lunch,Dinner,CabinNo,getCurrentDateWithoutTime(),IsPresent]);
     }
     catch(error)
@@ -26,5 +26,72 @@ router.post('/postAttendance',async(req,res)=>{
     }
 });
 
+router.get('/getShifts',async (req,res)=>{
+    try {
+        const [results] = await connection.query(getQueries.getByShift);
+        res.json(results);
+    } catch (err) {
+        console.error('Error retrieving attendance:', err.stack);
+        res.status(500).send({ error: "Internal server error." });
+    }
+});
+
+router.get('/getCabin',async (req,res)=>{
+    try {
+        const [results] = await connection.query(getQueries.getByCabin);
+        res.json(results);
+    } catch (err) {
+        console.error('Error retrieving attendance:', err.stack);
+        res.status(500).send({ error: "Internal server error." });
+    }
+});
+
+router.get('/getPresentEmployeeCount',async (req,res)=>{
+    try {
+        const [results] = await connection.query(getQueries.getpresenteecount);
+        res.json(results);
+    } catch (err) {
+        console.error('Error retrieving attendance:', err.stack);
+        res.status(500).send({ error: "Internal server error." });
+    }
+});
+
+router.get('/getAttendanceByDate/:date',async (req,res)=>{
+    const {date}=req.params
+    try {
+        const [results] = await connection.query(getQueries.getattendencebyDate,[date]);
+        res.json(results);
+    } catch (err) {
+        console.error('Error retrieving attendance:', err.stack);
+        res.status(500).send({ error: "Internal server error." });
+    }
+});
+
+router.get('/dinnercount', async (req, res) => {
+    try {
+        const [rows] = await connection.query(getQueries.getDinnercount);
+        res.json(rows);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+router.get('/breakfastcount', async (req, res) => {
+    try {
+        const [rows] = await connection.query(getQueries.getbreakfastcount);
+        res.json(rows);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+router.get('/lunchcount', async (req, res) => {
+    try {
+        const [rows] = await connection.query(getQueries.getLunchcount);
+        res.json(rows);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
 
 module.exports=router;
